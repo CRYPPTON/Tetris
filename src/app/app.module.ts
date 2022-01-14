74f70d9cfab50def3db655fdd43a96e102a891eb
nikola <nikola.nikkolic@gmail.com>
2022-01-14 12:10:37 +0100

    init project structures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,26 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    CoreModule,
+     HttpClientModule,
+     TranslateModule.forRoot({
+         loader: {
+             provide: TranslateLoader,
+             useFactory: HttpLoaderFactory,
+             deps: [HttpClient]
+         }
+     })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+
+
+const ngModules = [
+  MatButtonModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ...ngModules],
+  exports: [...ngModules],
+})
+export class MaterialModule { }
